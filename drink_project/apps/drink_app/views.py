8ab53b9f5012d1267df70a02809e97f7ff8bdb2b
Martin Martin <martinmartin@Martins-MacBook-Pro.local>
2019-11-19 12:58:29 -0800

    drink_infopage

--- drink_project/apps/drink_app/views.py
+++ drink_project/apps/drink_app/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'drink_app/index.html')
+
+def infopage(request):
+    return render(request, 'drink_app/drink.html')
